5275cd14a62d3e92ce0c53356bec5f632de55058
Uild Siqueira <usiqueira@mwneo.com>
2024-04-07 22:33:20 -0300

    Microservices

---
+++ src/gateway/storage/util.py
@@ -0,0 +1,30 @@
+import os
+import pika, json
+
+
+def upload(f, fs, channel, access):
+    try:
+        fid = fs.put(f)
+    except Exception as err:
+        print(err)
+        return f"internal server error (upload): {err}", 500
+
+    message = {
+        "video_fid": str(fid),
+        "mp3_fid": None,
+        "username": access["username"],
+    }
+
+    try:
+        channel.basic_publish(
+            exchange="",
+            routing_key=os.environ.get("VIDEO_QUEUE"),
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            ),
+        )
+    except Exception as err:
+        print(err)
+        fs.delete(fid)
+        return f"internal server error (publish): {err}", 500
